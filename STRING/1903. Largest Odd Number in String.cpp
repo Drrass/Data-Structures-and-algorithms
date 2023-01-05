488b6e0e7c2c2ba0a31b6eb488a638762e5ce18f
Himanshu Mehra <100959929+Drrass@users.noreply.github.com>
2023-01-06 03:31:03 +0530

    Create 1903. Largest Odd Number in String.cpp

---
+++ STRING/1903. Largest Odd Number in String.cpp
@@ -0,0 +1,11 @@
+https://leetcode.com/problems/largest-odd-number-in-string/
+class Solution {
+public:
+    string largestOddNumber(string num) {
+       for(int i=num.size()-1;i>=0;i--){
+           if(num[i]%2==0) num.pop_back();
+           else break;
+       } 
+       return num;
+    }
+};
