76a153fbdbd8bede80c3396f81d1b3e9a297cd96
Himanshu Mehra <100959929+Drrass@users.noreply.github.com>
2023-01-25 00:14:23 +0530

    Update 5_subsequence_equal_to_K.cpp

--- Recursion/5_subsequence_equal_to_K.cpp
+++ Recursion/5_subsequence_equal_to_K.cpp
@@ -29,3 +29,45 @@ using namespace std;
         Printf(0, v, 0, sum, arr, n);
     return 0;
 }
+
+if you want to print only one time the sum is equal to true then we will use bool
+
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+    bool Printf(int ind,vector<int> &v,int s,int sum,int arr[],int n){
+        if(ind==n){
+            if(s==sum){
+                for(auto it: v){
+                cout<<it<<" ";
+                return true;
+            }
+             cout<<endl;
+            }
+            return false;
+        }
+        v.push_back(arr[ind]);
+        s += arr[ind];
+        if(Printf(ind+1,v,s,sum,arr,n)==true){
+            return true;
+        }
+        v.pop_back();
+        s -= arr[ind];
+       if(Printf(ind+1,v,s,sum,arr,n)==true){
+            return true;
+        }
+        return false;
+        }
+        
+    int main()
+        {
+        int arr[]={1,2,1};
+        int n=3;
+        int sum=2;
+        vector<int> v;
+        Printf(0, v, 0, sum, arr, n);
+    return 0;
+}
+
